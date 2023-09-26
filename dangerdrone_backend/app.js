830400022de03946f4b349c92126eb874b6dd1dc
0x0leon <112933659+0x0leon@users.noreply.github.com>
2023-09-26 10:16:42 +0200

    Update: socket-connection working

--- dangerdrone_backend/app.js
+++ dangerdrone_backend/app.js
@@ -1,15 +1,95 @@
 const express = require('express')
+const Tello = require("tello-drone");
+const cors = require("cors")
 const app = express()
-const port = 3000
+
+app.use(cors)
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+const socketIOPort = 5001
+const expressPort = 3001
+
+
+
+console.log("test")
+// All option parameters are optional, default values shown
+const drone = new Tello({
+    host: "192.168.10.1",     // manually set the host.
+    port: "8889",             // manually set the port.
+    statePort: "8890",        // manually set the state port.
+    skipOK: false,            // dont send the OK message.
+})
+
+// Sends a command to the drone
 
 // socke io 
 
+var battery = 0
+
+drone.on("connection", () => {
+    console.log("Connected to drone");
+});
+
+var stateX = ""
+drone.on("state", state => {
+    // console.log("Recieved State > ", state);
+    io.emit("droneData", state)
+    stateX = state
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on("dataX", (data) => {
+        console.log(data)
+
+        
+    })
+
+    socket.on("up", (data) => {
+        drone.send("up", 10)
+    })
+
+    socket.on("down", (data) => {
+        drone.send("down", 10)
+    })
+
+    socket.on("emergency", (data) => {
+        drone.send("emergency")
+    })
+
+
+});
+
+
+
+
 app.get('/', (req, res) => {
-  res.json({message:'test'})
+    drone.send("battery?")
+
+    res.json({ batteryy: battery })
 })
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/command/:command', function () {
+
+})
+
+
+
+
+io.listen(socketIOPort, () => {
+    console.log(`listening on *: ${socketIOPort}`);
+});
+
+app.listen(expressPort, () => {
+    console.log(`Example app listening on port ${expressPort}`)
 })
--- dangerdrone_frontend/src/App.js
+++ dangerdrone_frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client'
+import { Button, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "http://localhost:5001"
+const socket = io(url)
+
+
+const App = () => {
+
+	const [connected, setConnected] = useState(false)
+	const [droneData, setDroneData] = useState("")
+
+	const up = (x) => {
+		socket.emit('up', x)
+	}
+
+	const down = () => {
+		console.log("down")
+		socket.emit('down', 10)
+	}
+
+	const emergency = () => {
+		socket.emit('emergency', "")
+	}
+
+	useEffect(() => {
+
+		socket.on("connect", (data) => {
+			console.log("connected")
+			setConnected(true)
+		})
+
+		socket.on("droneData", (data) => {
+			setDroneData(data)
+			// console.log(data)
+		})
+	})
+
+
+
+	return (
+		<Container className='m-5'>
+			<h1>test</h1>
+			<h4>drone battery {droneData['bat']}</h4>
+			<h4>drone X {droneData["x"]}</h4>
+			<h4>drone Y {droneData["y"]}</h4>
+			<h4>drone Z {droneData["z"]}</h4>
+			<h4>drone roll {droneData["roll"]}</h4>
+			<h4>drone baro {droneData["baro"]}</h4>
+			<Button onClick={up}>up</Button>
+			<Button onClick={down}>down</Button>
+			<Button onClick={emergency}>stop</Button>
+		</Container>
+	);
 }
 
 export default App;
